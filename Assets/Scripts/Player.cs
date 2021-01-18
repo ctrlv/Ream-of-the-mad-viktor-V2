598a18e32e02e0d26104afae55c22a685b53d8ea
Matheus Medina <matheusmvo@hotmail.com>
2021-01-18 14:00:56 -0300

    Pintão

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed= 5f;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        Input.GetAxisRaw("horizontal");
+    }
+
+    void FixedUpdate(){
+
+
+    }
+}
