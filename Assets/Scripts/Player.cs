fad51a0d096954caa5e977460862252f9c7f731e
Matheus Medina <matheusmvo@hotmail.com>
2021-01-18 14:50:13 -0300

    movimentação e camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float moveSpeed= 5f;
+    public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
         //input
-        Input.GetAxisRaw("horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate(){
-
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime );
 
     }
 }
